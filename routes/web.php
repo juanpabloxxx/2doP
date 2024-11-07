505f5d58a49297b529ced880acb3e7c6389344fb
“Juan”pabloxxx <122044165@upq.edu.mx>
2024-11-06 20:24:29 -0600

    aqui iniciamos nuestro proyecto 2dop

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-
-    use App\Http\Controllers\RegistroController;
 });
 
 Route::get('/dashboard', function () {
